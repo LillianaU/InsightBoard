cf43d849be40da2c01f627312acf72471e238ca9
Lilly <lillianauribe@gmail.com>
2026-09-07 23:01:19 -0500

    redirect root to API docs

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
+
+def redirect_to_docs(request):
+    return redirect('schema-swagger-ui')
+
+
 urlpatterns = [
+    path('', redirect_to_docs, name='root'),
     path('health/', views.health_check, name='health-check'),
 ]
